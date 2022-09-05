32bf078dd2bd57bb8068c3ba6158a83fc1922598
Tova Jertfelt <tovizitan@gmail.com>
2022-09-05 13:50:28 +0200

    work in progress

--- src/components/ReactPost.jsx
+++ src/components/ReactPost.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Loading from "./Loading"
 import {BulletinBoard} from "./BulletinBoard";
+import PostLiked from "./PostLiked";
 
 const ReactPost = () => {
   const [newPage, setNewPage] = useState(true)
@@ -30,6 +31,7 @@ const ReactPost = () => {
   }
 
   const showMeAnswer = () => {
+    //if sats - kolla id och hur man skickar
     setIsLoading(true);
     setShowButt(false);
  };
@@ -50,12 +52,63 @@ const ReactPost = () => {
   <article className="react-post grid">
     <span className="grid-item">
     <h2 className="question">Fråga #{BulletinBoard[0].id}: {BulletinBoard[0].question}</h2>
+   
     {showButt ? 
     <button className="reveal"
     onClick={showMeAnswer}> SVAR</button> :null }
      {isLoading ? <Loading /> : null }
      {revealAnswer ? <h3 className="answer">{BulletinBoard[0].answer}</h3> : null }
+     <span>
+      <p><PostLiked></PostLiked></p>
+    </span>
      </span>
+
+     <span className="grid-item">
+    <h2 className="question">Fråga #{BulletinBoard[1].id}: {BulletinBoard[1].question}</h2>
+    {showButt ? 
+    <button className="reveal"
+    onClick={showMeAnswer}> SVAR</button> :null }
+     {isLoading ? <Loading /> : null }
+     {revealAnswer ? <h3 className="answer">{BulletinBoard[1].answer}</h3> : null }
+     </span>
+     <span className="grid-item">
+    <h2 className="question">Fråga #{BulletinBoard[2].id}: {BulletinBoard[2].question}</h2>
+    {showButt ? 
+    <button className="reveal"
+    onClick={showMeAnswer}> SVAR</button> :null }
+     {isLoading ? <Loading /> : null }
+     {revealAnswer ? <h3 className="answer">{BulletinBoard[2].answer}</h3> : null }
+     </span>
+     <span className="grid-item">
+    <h2 className="question">Fråga #{BulletinBoard[3].id}: {BulletinBoard[3].question}</h2>
+    {showButt ? 
+    <button className="reveal"
+    onClick={showMeAnswer}> SVAR</button> :null }
+     {isLoading ? <Loading /> : null }
+     {revealAnswer ? <h3 className="answer">{BulletinBoard[3].answer}</h3> : null }
+     </span>
+     <span className="grid-item">
+    <h2 className="question">Fråga #{BulletinBoard[4].id}: {BulletinBoard[4].question}</h2>
+    {showButt ? 
+    <button className="reveal"
+    onClick={showMeAnswer}> SVAR</button> :null }
+     {isLoading ? <Loading /> : null }
+     {revealAnswer ? <h3 className="answer">{BulletinBoard[4].answer}</h3> : null }
+     </span>
+  </article>
+  <article>
+    <div className="Further-Questions"> 
+      <h2>Har du någon mer fråga?</h2>
+    {/* //* Inte programmerat färdigt detta ännu:  */}
+    <form>
+      <label>
+      Ja, jag undrar...</label>
+      <textarea
+      placeholder="Vad står React för?">
+      </textarea>
+      <input type="submit" value="Fråga!"/>
+      <p>Obs: frågorna kommer besvaras i mån av tid, så fort det finns ett svar så kommer det synas här på hemsidan.</p>
+      </form></div>
   </article>
   <button className="restart" onClick={restart}> Börja om</button>
   </div> );
