811d7af8cf4f6d5ec9f9485d50c0583d0c12e806
Tova Jertfelt <tovizitan@gmail.com>
2022-09-05 14:00:45 +0200

    fortsätter på den enklare lösningen först

--- src/components/PostLiked.jsx
+++ src/components/PostLiked.jsx
@@ -1,4 +1,19 @@
+import {useState, useEffect} from "react"
 const  PostLiked = () => {
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [like, setLike] = useState("./src/img/icons8-thumbs-up-96.png")
+
+ useEffect(() => {
+  if (isLoading) {
+    setTimeout(() => {
+    setIsLoading(false);
+    setLike("")
+  }, 2000);
+  }
+}, [isLoading]);
+
+
   return ( <div></div> );
 }
  
--- src/components/ReactPost.jsx
+++ src/components/ReactPost.jsx
@@ -4,47 +4,8 @@ import {BulletinBoard} from "./BulletinBoard";
 import PostLiked from "./PostLiked";
 
 const ReactPost = () => {
-  const [newPage, setNewPage] = useState(true)
-  const [questions, setQuestions] = useState("QWESTION");
   const [showButt, setShowButt] = useState(true)
-  const [questItem, setQuestItem] = useState(null);
-  const [revealAnswer, setRevealAnswer] = useState(false);
-  const [answer, setAnswer] = useState("ANSWER MEEEEE");
-  const [isLoading, setIsLoading] = useState(false);
-
- const allQuestions = BulletinBoard.map( item => item.question)
-
-
-  useEffect(() => {
-    if (newPage){ 
-      const allQuestions = BulletinBoard.map( item => item.question)
-      setQuestions(allQuestions)
-
-    }
-  }, [newPage])
  
-
-
-  const restart = () => {
-  setShowButt(true);
-  setRevealAnswer(false);
-  }
-
-  const showMeAnswer = () => {
-    //if sats - kolla id och hur man skickar
-    setIsLoading(true);
-    setShowButt(false);
- };
- 
- useEffect(() => {
-  if (isLoading) {
-    setTimeout(() => {
-    setIsLoading(false);
-    setRevealAnswer(true);
-  }, 2000);
-  }
-}, [isLoading]);
-
 
 
   return ( 
@@ -53,47 +14,38 @@ const ReactPost = () => {
     <span className="grid-item">
     <h2 className="question">Fråga #{BulletinBoard[0].id}: {BulletinBoard[0].question}</h2>
    
-    {showButt ? 
-    <button className="reveal"
-    onClick={showMeAnswer}> SVAR</button> :null }
      {isLoading ? <Loading /> : null }
-     {revealAnswer ? <h3 className="answer">{BulletinBoard[0].answer}</h3> : null }
+      <h3 className="answer">{BulletinBoard[0].answer}</h3> : null 
      <span>
+      <button className="reveal"
+      onClick={like}></button>
       <p><PostLiked></PostLiked></p>
     </span>
      </span>
 
      <span className="grid-item">
     <h2 className="question">Fråga #{BulletinBoard[1].id}: {BulletinBoard[1].question}</h2>
-    {showButt ? 
-    <button className="reveal"
-    onClick={showMeAnswer}> SVAR</button> :null }
+   
      {isLoading ? <Loading /> : null }
-     {revealAnswer ? <h3 className="answer">{BulletinBoard[1].answer}</h3> : null }
+     <h3 className="answer">{BulletinBoard[1].answer}</h3> 
      </span>
      <span className="grid-item">
     <h2 className="question">Fråga #{BulletinBoard[2].id}: {BulletinBoard[2].question}</h2>
-    {showButt ? 
-    <button className="reveal"
-    onClick={showMeAnswer}> SVAR</button> :null }
+    
      {isLoading ? <Loading /> : null }
-     {revealAnswer ? <h3 className="answer">{BulletinBoard[2].answer}</h3> : null }
+     <h3 className="answer">{BulletinBoard[2].answer}</h3> 
      </span>
      <span className="grid-item">
     <h2 className="question">Fråga #{BulletinBoard[3].id}: {BulletinBoard[3].question}</h2>
-    {showButt ? 
-    <button className="reveal"
-    onClick={showMeAnswer}> SVAR</button> :null }
+    
      {isLoading ? <Loading /> : null }
-     {revealAnswer ? <h3 className="answer">{BulletinBoard[3].answer}</h3> : null }
+      <h3 className="answer">{BulletinBoard[3].answer}</h3> : 
      </span>
      <span className="grid-item">
     <h2 className="question">Fråga #{BulletinBoard[4].id}: {BulletinBoard[4].question}</h2>
-    {showButt ? 
-    <button className="reveal"
-    onClick={showMeAnswer}> SVAR</button> :null }
+    
      {isLoading ? <Loading /> : null }
-     {revealAnswer ? <h3 className="answer">{BulletinBoard[4].answer}</h3> : null }
+      <h3 className="answer">{BulletinBoard[4].answer}</h3> : null 
      </span>
   </article>
   <article>
